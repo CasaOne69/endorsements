b2015b80ffbd6fd7b7dd17d2c4e55598e03b2143
CasaOne69 <oficina@casalock.es>
2023-07-30 19:07:54 +0200

    Fire Base Connected

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+
+const appSettings = {
+    databaseURL: "https://endorsements-b475e-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+const app = initializeApp(appSettings) 
+const database = getDatabase(app) 
+const endorsementsInDB = ref(database, "endorsement")
+const fromInDB = ref(database, "from")
+//push(endorsementsInDB, "CockSuckers") 
+//push(fromInDB, "ArseLickers") 
