f59d04198d819e80badd0bae690edfca752e8a1d
CasaOne69 <oficina@casalock.es>
2023-08-01 15:26:49 +0200

    In progress

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-
-
+//Firebase connections
 const appSettings = {
     databaseURL: "https://endorsements-b475e-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 const app = initializeApp(appSettings) 
 const database = getDatabase(app) 
 const endorsementsInDB = ref(database, "endorsement")
-const fromInDB = ref(database, "from")
-//push(endorsementsInDB, "CockSuckers") 
-//push(fromInDB, "ArseLickers") 
+
+//Elements from dom
+const endorseEl = document.getElementById("endoreElbox")
+const fromEl = document.getElementById("from-name")
+const toEl = document.getElementById("to-el")
+const publishBtn = document.getElementById("publish-endorsement")
+
+//function to fetch input values and push to DB
+publishBtn.addEventListener ("click", function (){
+    let fullEndorsement = `To ${toEl.value} ${endorseEl.value} From ${fromEl.value}`
+    console.log(fullEndorsement)
+    push(endorsementsInDB, fullEndorsement) 
+})
